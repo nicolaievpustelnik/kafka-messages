4ee2501c78bf31e3b7d962037a25950ecfac3377
Nicolaiev Brito Pustelnik <nicolaievbrito@gmail.com>
2024-02-27 15:36:04 -0300

    Create kafka messages

---
+++ provider/src/main/java/com/kafka/provider/ProviderApplication.java
@@ -0,0 +1,22 @@
+package com.kafka.provider;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class ProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("topic-message", "Hello world with kafka!");
+		};
+	}
+}
